38d668941226a2575211098c7d4fb2e30910297c
Creed Petitt <creedpetitt@gmail.com>
2025-09-30 22:16:53 -0500

    add image gallery

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { Context } from '../../context/ContextProvider';
+import './ImageGallery.css';
+
+const ImageGallery = () => {
+    const { generatedImages } = useContext(Context);
+
+    return (
+        <div className="image-gallery-container">
+            <div className="gallery-header">
+                <h1>Image Gallery</h1>
+                <p>{generatedImages.length} images generated</p>
+            </div>
+            <div className="image-grid">
+                {generatedImages.map((image, index) => (
+                    <div key={index} className="grid-item">
+                        <img src={image.imageUrl} alt={image.prompt} />
+                        <div className="grid-item-info">
+                            <p>{image.prompt}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ImageGallery;
